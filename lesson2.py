1af06ca4647c3b628e65f8f9ee3e82834e942d44
Ramirez <vmramirez5@cps.k12.il.us>
2024-08-30 15:06:31 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,49 @@
+#create a print function that prints out "Hello World"
+# print ("Hello, World!")
+#ask the user for the day of the week
+# day_of_week = input("What day of the week is it?")
+# print("Toady is " + day_of_week)
+# concatenation is when you add two strings together
+# Using a plus sign (+)
+# movies_this_week = input("What are you watching this week?")
+# print("I am watching " + movies_this_week + " this week.")
+# mood = input("How are you feeling toady?")
+# print("I am feeling " + mood)
+
+# Data types for vairables in python
+
+# strings - text
+# # name = "John" # this is a string data type whenever it is wrapped in poutes nit is a string
+# # year = "2024"
+
+# # integers - whole numbers
+# year =2024 # this is an integer data type don't wrap it in qoutes
+# yearFourFromNow = 2028
+# subtract = yearFourFromNow - year
+# print(subtract)
+
+# # Floats - decimal numbers
+# priceBigMac = 3.99 # this is a float data type
+# priceDoublePounder = 4.99
+# totalPrice = priceBigMac + priceDoublePounder
+# print(totalPrice)
+
+# # booleans -True or False
+# isRaining =False #this is boolean type data
+# print(isRaining)
+
+# # lists- a collection of items
+# groceries = ["apples " + "bananas " + "carrots"]
+# print(groceries)
+
+#Challenge #1 
+movies = ["Beetlejuice Beetlejuice " + "Alien: Romulous " + "Deadpool and Wolverine"]
+movie_option = input("What movie would you like to see?")
+movie_time = input(""When would you like to watch " + movie_option "?"")
+print(movie_option + "at" + movie_time + "will cost $6 per person.")
+movie_price = 6 
+adress = "8352 mars street"
+dinner_time = "10 am"
+number_of_people = input("How many people are going?")
+# type conversion converting from one dat type to another
+print("We are going to see" + movie_option + "at" + movie_time + "and then eat dinner at" + adress + "at" + dinner_time + "with " + number_of_people + "going")
